2bb43f19f9b5c92b6cee66d44e44a4f608f6fb6e
Chamindu <ransikamax2008@gmail.com>
2025-02-27 22:45:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "Enter your session ID",
-  MONGODB: process.env.MONGODB || "Enter your mongoDB public URL",
-  OWNER_NUM: process.env.OWNER_NUM || "947xxxxxxxx",
+  SESSION_ID: process.env.SESSION_ID || "CuQWXJbI#6QhD628MrfKLLFhch5VUl3Q9Q6WBgyTnaK3xJiehAbg",
+  MONGODB: process.env.MONGODB || "mongodb://mongo:gypcdIlYLLwTrZUhrdhhNLtAQkzVBOQh@switchback.proxy.rlwy.net:45570",
+  OWNER_NUM: process.env.OWNER_NUM || "94783314361",
 };
